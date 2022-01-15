bce7248883239d99e98d41db558d19c4dc456b97
anott03 <amitav_nott@ryecountryday.org>
2022-01-15 15:52:01 -0500

    things

--- client/src/features/signin/SignIn.tsx
+++ client/src/features/signin/SignIn.tsx
@@ -8,9 +8,9 @@ export function SignIn({signUp}: {signUp: boolean}) {
   let navigate = useNavigate();
   let dispatch = useAppDispatch();
   let user = useAppSelector(selectUser); let [errorMessage, setErrorMessage] = useState(null);
-  let [autofillUsername, setAutofillUsername] = useState(false);
 
-  const cardStyle: CSSProperties = { backgroundColor: 'white',
+  const cardStyle: CSSProperties = {
+      backgroundColor: '#f1f7ed',
       width: "30%",
       height: "50%",
       maxWidth: "50rem",
@@ -23,6 +23,7 @@ export function SignIn({signUp}: {signUp: boolean}) {
     };
 
   const pageStyles: CSSProperties = {
+    backgroundColor: '#f1f7ed',
     width: "100vw",
     height: "100vh",
     position: "absolute", top: 0,
@@ -30,18 +31,11 @@ export function SignIn({signUp}: {signUp: boolean}) {
     left: 0,
     right: 0,
     display: "flex",
-  }
-
-  //TODO: figure out why autofilling username after signup isn't working
-  function checkAutofill() {
-    console.log(user);
-    if (user && !user.signedIn) {
-      setAutofillUsername(true);
-    }
-  }
+  };
 
   useEffect(() => {
-    checkAutofill();
+    //TODO: we cannot let this stay as is; probably just don't allowed logged
+    // in users to access this route at all
     dispatch(setUser({username: null, signedIn: false}));
   }, []);
 
@@ -53,7 +47,7 @@ export function SignIn({signUp}: {signUp: boolean}) {
     let pwd = encodeURI(e.target.password.value);
 
     let url = signUp ? `http://127.0.0.1:8888/api/v1/register?name=${username}&pwd=${pwd}`
-                     : `http://127.0.0.1:8888/api/v1/login?name=${username}&pwd=${pwd}`
+                     : `http://127.0.0.1:8888/api/v1/login?name=${username}&pwd=${pwd}`;
 
     httpGetAsync(url, (res: string) => {
       let json = JSON.parse(res);
@@ -73,7 +67,6 @@ export function SignIn({signUp}: {signUp: boolean}) {
 
     e.target.username.value = "";
     e.target.username.password = "";
-    checkAutofill();
   }
 
   return (
@@ -87,10 +80,10 @@ export function SignIn({signUp}: {signUp: boolean}) {
             { errorMessage !== null && <div className="mt-6 bg-red-200" style={{width: "100%", padding: "20px"}}>
               <p>{errorMessage}</p>
             </div> }
-            <input id="username" className="mt-6 ml-0 placeholder:italic placeholder:text-slate-400 block w-full border border-slate-300 rounded-md py-2 pl-1.5 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" style={{backgroundColor: "#ebebeb"}} placeholder="Username" type="text" name="search" defaultValue={(!signUp&&autofillUsername) ? user.username : ""}/>
-            <input id="password" className="mt-6 ml-0 placeholder:italic placeholder:text-slate-400 block w-full border border-slate-300 rounded-md py-2 pl-1.5 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" style={{backgroundColor: "#ebebeb"}} placeholder="Password" type="password" name="search"/>
-            {signUp ? <small className="mt-6"> Already have an account? <button onClick={() => navigate("/signin")} className="text-blue-600 hover:cursor-pointer font-bold">Login.</button></small>
-                    : <small className="mt-6"> Don't have an account? <button onClick={() => navigate("/signup")} className="text-blue-600 hover:cursor-pointer font-bold">Sign Up.</button></small> }
+            <input id="username" className="mt-6 ml-0 placeholder:italic placeholder:text-slate-400 block w-full border border-slate-300 rounded-md py-2 pl-1.5 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" style={{backgroundColor: "#e6f0e3"}} placeholder="Username" type="text" name="search"/>
+            <input id="password" className="mt-6 ml-0 placeholder:italic placeholder:text-slate-400 block w-full border border-slate-300 rounded-md py-2 pl-1.5 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" style={{backgroundColor: "#e6f0e3"}} placeholder="Password" type="password" name="search"/>
+            {signUp ? <small className="mt-6"> Already have an account? <button onClick={() => navigate("/signin")} className="text-slate-600 hover:cursor-pointer font-bold">Login.</button></small>
+                    : <small className="mt-6"> Don't have an account? <button onClick={() => navigate("/signup")} className="text-slate-600 hover:cursor-pointer font-bold">Sign Up.</button></small> }
             <button className="border border-solid border-gray-600 mt-3 p-1.5 pl-5 pr-5 rounded-md hover:bg-gray-100 font-bold" type="submit">Continue</button>
           </form>
         </div>
