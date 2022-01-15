47e2bf33be9d2e247f2dac500bfa51ac5e582942
anott03 <amitav_nott@ryecountryday.org>
2022-01-15 13:00:59 -0500

    v1 of signup

--- client/src/features/signup/SignUp.tsx
+++ client/src/features/signup/SignUp.tsx
@@ -1,5 +1,5 @@
 import React, { CSSProperties } from 'react';
-import './SignUp.css';
+import { httpGetAsync } from '../../utils';
 
 export function SignUp() {
   const cardStyle: CSSProperties = {
@@ -27,8 +27,16 @@ export function SignUp() {
 
   function createAccount(e: any): void {
     e.preventDefault();
+    let username = encodeURI(e.target.username.value);
+    let pwd = encodeURI(e.target.password.value);
 
-    console.log(e.target.username, e.target.password);
+    let url = `http://127.0.0.1:8888/api/v1/register?name=${username}&pwd=${pwd}`
+    httpGetAsync(url, (res: string) => {
+      console.log(res);
+    });
+
+    e.target.username.value = "";
+    e.target.username.password = "";
   }
 
   return (
@@ -39,9 +47,9 @@ export function SignUp() {
             <h1 className="text-3xl font-bold">
               Create an Account
             </h1>
-            <input className="mt-6 ml-0 placeholder:italic placeholder:text-slate-400 block w-full border border-slate-300 rounded-md py-2 pl-1.5 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" style={{backgroundColor: "#ebebeb"}} placeholder="Username" type="text" name="search"/>
-            <input className="mt-6 ml-0 placeholder:italic placeholder:text-slate-400 block w-full border border-slate-300 rounded-md py-2 pl-1.5 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" style={{backgroundColor: "#ebebeb"}} placeholder="Password" type="password" name="search"/>
-            <small className="mt-6"> Already have an account? <a className="text-blue-600 hover:cursor-pointer font-bold">Login.</a></small>
+            <input id="username" className="mt-6 ml-0 placeholder:italic placeholder:text-slate-400 block w-full border border-slate-300 rounded-md py-2 pl-1.5 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" style={{backgroundColor: "#ebebeb"}} placeholder="Username" type="text" name="search"/>
+            <input id="password" className="mt-6 ml-0 placeholder:italic placeholder:text-slate-400 block w-full border border-slate-300 rounded-md py-2 pl-1.5 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" style={{backgroundColor: "#ebebeb"}} placeholder="Password" type="password" name="search"/>
+            <small className="mt-6"> Already have an account? <button className="text-blue-600 hover:cursor-pointer font-bold">Login.</button></small>
             <button className="border border-solid border-gray-600 mt-3 p-1.5 pl-5 pr-5 rounded-md hover:bg-gray-100 font-bold" type="submit">Continue</button>
           </form>
         </div>
