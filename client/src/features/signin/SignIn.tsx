0a982895bf3e5d2a583278f741da3bd6ba155d20
anott03 <amitav_nott@ryecountryday.org>
2022-01-15 15:34:58 -0500

    fix(all the bugs): stuff

--- client/src/features/signin/SignIn.tsx
+++ client/src/features/signin/SignIn.tsx
@@ -7,8 +7,7 @@ import { useNavigate } from 'react-router';
 export function SignIn({signUp}: {signUp: boolean}) {
   let navigate = useNavigate();
   let dispatch = useAppDispatch();
-  let user = useAppSelector(selectUser);
-  let [errorMessage, setErrorMessage] = useState(null);
+  let user = useAppSelector(selectUser); let [errorMessage, setErrorMessage] = useState(null);
   let [autofillUsername, setAutofillUsername] = useState(false);
 
   const cardStyle: CSSProperties = { backgroundColor: 'white',
@@ -43,6 +42,7 @@ export function SignIn({signUp}: {signUp: boolean}) {
 
   useEffect(() => {
     checkAutofill();
+    dispatch(setUser({username: null, signedIn: false}));
   }, []);
 
   function submit(e: any): void {
@@ -80,7 +80,7 @@ export function SignIn({signUp}: {signUp: boolean}) {
     <div style={pageStyles}>
       <div style={cardStyle}>
         <div style={{flex: 1, display: "flex", alignItems: "center", width: "100%"}}>
-          <form onSubmit={submit} style={{margin: "10px", display: "flex", flexDirection: "column", alignItems: "start", flex: 1}}>
+          <form style={{margin: "10px", display: "flex", flexDirection: "column", alignItems: "start", flex: 1}} onSubmit={submit}>
             <h1 className="text-3xl font-bold">
               {signUp ? "Create an Account" : "Sign In"}
             </h1>
@@ -89,8 +89,8 @@ export function SignIn({signUp}: {signUp: boolean}) {
             </div> }
             <input id="username" className="mt-6 ml-0 placeholder:italic placeholder:text-slate-400 block w-full border border-slate-300 rounded-md py-2 pl-1.5 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" style={{backgroundColor: "#ebebeb"}} placeholder="Username" type="text" name="search" defaultValue={(!signUp&&autofillUsername) ? user.username : ""}/>
             <input id="password" className="mt-6 ml-0 placeholder:italic placeholder:text-slate-400 block w-full border border-slate-300 rounded-md py-2 pl-1.5 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" style={{backgroundColor: "#ebebeb"}} placeholder="Password" type="password" name="search"/>
-            {signUp ? <small className="mt-6"> Already have an account? <button className="text-blue-600 hover:cursor-pointer font-bold">Login.</button></small>
-                    : <small className="mt-6"> Don't have an account? <button className="text-blue-600 hover:cursor-pointer font-bold">Sign Up.</button></small> }
+            {signUp ? <small className="mt-6"> Already have an account? <button onClick={() => navigate("/signin")} className="text-blue-600 hover:cursor-pointer font-bold">Login.</button></small>
+                    : <small className="mt-6"> Don't have an account? <button onClick={() => navigate("/signup")} className="text-blue-600 hover:cursor-pointer font-bold">Sign Up.</button></small> }
             <button className="border border-solid border-gray-600 mt-3 p-1.5 pl-5 pr-5 rounded-md hover:bg-gray-100 font-bold" type="submit">Continue</button>
           </form>
         </div>
--- client/src/features/user/userSlice.ts
+++ client/src/features/user/userSlice.ts
@@ -2,11 +2,13 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '../../app/store';
 
 interface UserState {
-  user: any;
+  user: { username: string, signedIn: boolean } | null; 
+  books: {};
 }
 
 const initialState: UserState = {
   user: null,
+  books: {},
 };
 
 export const userSlice = createSlice({
@@ -15,15 +17,19 @@ export const userSlice = createSlice({
   reducers: {
     setUser: (state, action: PayloadAction<any>) => {
       state.user = action.payload;
+    },
+    setUserBooks: (state, action: PayloadAction<any>) => {
+      state.books = action.payload;
     }
   },
 });
 
-export const { setUser } = userSlice.actions;
+export const { setUser, setUserBooks } = userSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
 export const selectUser = (state: RootState) => state.userState.user;
+export const selectUserBooks = (state: RootState) => state.userState.books;
 
 export default userSlice.reducer;
